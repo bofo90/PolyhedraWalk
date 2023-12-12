dd0d82c527dcf51f773054aff54ad5e88125b079
bofo90 <f.agustin@gmail.com>
2023-12-12 18:46:04 +0100

    make gif maker with no return

--- make_gif.py
+++ make_gif.py
@@ -5,7 +5,7 @@ from PIL import Image
 i, j = 17, 0
 
 
-def make_gif(frame_folder):
+def make_gif_w_return(frame_folder):
     files = glob.glob(f"{frame_folder}/*.png")
     sorted_files = sorted(files)
     frames = [Image.open(image) for image in sorted_files[:-20] + files + sorted_files[-20::-1]]
@@ -20,5 +20,22 @@ def make_gif(frame_folder):
     )
 
 
+def make_gif(frame_folder):
+    files = glob.glob(f"{frame_folder}/*.png")
+    files.sort()
+    frames = [Image.open(image) for image in files]
+    frame_one = frames[0]
+    frame_one.save(
+        "Polycircle/test_angle_85_1.gif",
+        format="GIF",
+        append_images=frames[1:],
+        save_all=True,
+        optimize=False,
+        loop=0,
+        # duration=100,
+    )
+
+
 if __name__ == "__main__":
-    make_gif(f"/home/agustin/Programming/CoverPhDThesis/Polyinsects/alpha_{i}_{j}")
+    # make_gif_w_return(f"/home/agustin/Programming/CoverPhDThesis/Polyinsects/alpha_{i}_{j}")
+    make_gif("/home/agustin/Programming/CoverPhDThesis/Polycircle/test_angle_85_1")
